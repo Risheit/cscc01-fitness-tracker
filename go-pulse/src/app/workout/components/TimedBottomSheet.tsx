15f0776d90ae6706a3dc44f277a4c65da2db53d4
AashirM2004 <133847421+AashirM2004@users.noreply.github.com>
2025-03-07 17:54:36 -0500

    finished adding audio cues to the timer portion of workouts (#25)
    * finished adding audio cues to the timer portion of workouts
    
    * cleaned up comments

--- go-pulse/src/app/workout/components/TimedBottomSheet.tsx
+++ go-pulse/src/app/workout/components/TimedBottomSheet.tsx
@@ -24,6 +24,7 @@ export default function TimedBottomSheet({
     `${data.mins}m 0s`
   );
   const remainingMillis = useRef(data.mins! * 60 * 1000);
+  const audioRef = useRef(new Audio('/Timer.mp3'));
 
   const onLikedButtonClicked = () => {
     setIsLiked(!isLiked);
@@ -45,7 +46,7 @@ export default function TimedBottomSheet({
         if (remainingMillis.current <= 0) {
           setDisplayTimeRemaining(`All done!`);
           setIsFinished(true);
-          clearInterval(timerId);
+          audioRef.current.play();
           return;
         }
 
