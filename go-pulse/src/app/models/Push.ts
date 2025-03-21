043c89983aab3bab10d0e2e33102133036a5b2a0
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2025-03-21 13:52:42 -0400

    Add show notifications button

--- go-pulse/src/app/models/Push.ts
+++ go-pulse/src/app/models/Push.ts
@@ -1,8 +1,57 @@
+// Code taken and modified from https://github.com/ameerezae/web-push-nextjs/
+
 import webpush from 'web-push';
 
 export interface UserSubscription {
   [userId: number]: webpush.PushSubscription;
-} 
+}
 
 export const subscriptions: UserSubscription = {};
 
+export function notificationUnsupported(): boolean {
+  let unsupported = false;
+  if (
+    !('serviceWorker' in navigator) ||
+    !('PushManager' in window) ||
+    !('showNotification' in ServiceWorkerRegistration.prototype)
+  ) {
+    unsupported = true;
+  }
+  return unsupported;
+}
+
+export async function trySubscribeToNotifications(
+  state: NotificationPermission
+) {
+  switch (state) {
+    case 'denied':
+      return true;
+    case 'default':
+      return false;
+    case 'granted':
+      try {
+        await navigator.serviceWorker.register('./sw.js');
+        const registration = await navigator.serviceWorker.ready;
+        const subscription = await registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+        });
+        console.info(
+          'Subscribed for push notifications',
+          subscription.toJSON()
+        );
+        const res = await fetch('/api/web-push/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ subscription }),
+        });
+        console.log('register completed', await res.json());
+        return true;
+      } catch (e) {
+        console.log('could not subscribe: ', e);
+        return false;
+      }
+  }
+}
