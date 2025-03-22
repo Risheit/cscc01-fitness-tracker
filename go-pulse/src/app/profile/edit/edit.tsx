5fe98a8165ce5a6e5fee9a89a352b67b169fc0ea
Muhammad Bilal <bilal29029@gmail.com>
2025-03-21 22:03:08 -0400

    Refactor: Remove commented-out code

--- go-pulse/src/app/profile/edit/edit.tsx
+++ go-pulse/src/app/profile/edit/edit.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-//import React, { useState } from "react";
+import { useRouter } from "next/navigation";;
 import React, { useEffect, useState } from "react";
 
 export default function EditProfile() {
