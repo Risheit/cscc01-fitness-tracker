c0900a36444e91aa26f0032c6289a7fde319aac8
Kazi <nafizul02@gmail.com>
2025-03-21 21:17:27 -0400

    Implemented workout link sharing

--- go-pulse/src/app/models/Workout.ts
+++ go-pulse/src/app/models/Workout.ts
@@ -15,6 +15,7 @@ export interface ExerciseData {
   mins?: number;
   sets?: number;
   reps?: number;
+  dayOfWeek?: string; // Add dayOfWeek to the interface
 }
 
 export type WorkoutState = 'start' | 'paused' | 'running' | 'end';
@@ -40,9 +41,12 @@ export async function getWorkoutPlan(planId: number): Promise<ExerciseData[]> {
     w.exercise_type AS "type",
     w.mins,
     w.sets,
-    w.reps
-  FROM workout_exercises AS w INNER JOIN exercises AS e ON w.name = e.name
-  WHERE workout_id = $1
+    w.reps,
+    wd.day_of_week AS "dayOfWeek" -- Include day_of_week in the query
+  FROM workout_exercises AS w
+  INNER JOIN exercises AS e ON w.name = e.name
+  INNER JOIN workout_days AS wd ON w.workout_day_id = wd.id
+  WHERE w.workout_id = $1
   ORDER BY w.position ASC`,
     [planId]
   );
