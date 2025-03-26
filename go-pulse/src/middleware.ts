041c3b158190a32b09350d706b2a4df4e40c78c5
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2025-03-26 03:48:29 -0400

    Try fix npm build not working

--- go-pulse/src/middleware.ts
+++ go-pulse/src/middleware.ts
@@ -2,14 +2,20 @@ import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
+  console.log('req', req);
+  console.log('reqUrl', req.url);
+  console.log('redirect url', new URL('/api/check-auth', req.url));
+
   const res = await fetch(new URL('/api/check-auth', req.url), {
     headers: { Cookie: (await cookies()).toString() },
     credentials: 'include',
   });
 
   if (res.ok) {
+    console.log('success');
     return NextResponse.next();
   } else {
+    console.log('failed');
     return NextResponse.redirect(new URL('/login', req.url));
   }
 }
